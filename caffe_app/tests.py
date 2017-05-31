9adedc741899c9d9ffb8a6f4320f5562a58d227d
utsavgarg <utsav1996@ymail.com>
2017-05-31 16:09:33 +0530

    Fixed indentation errors due to tabs

--- caffe_app/tests.py
+++ caffe_app/tests.py
@@ -4,12 +4,13 @@ import unittest
 import json
 import os
 
-class UploadTest(unittest.TestCase):
-		def setUp(self):
-				self.client = Client()
 
-		def test_details(self):
-				sample_file = open(os.path.join(settings.BASE_DIR,'example','GoogleNet.prototxt'), 'r')
-				response = self.client.post('/caffe/import', {'file': sample_file})
-				response = json.loads(response.content)
-				print 'Test {:}!'.format(response['result'])
+class UploadTest(unittest.TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_details(self):
+        sample_file = open(os.path.join(settings.BASE_DIR, 'example', 'GoogleNet.prototxt'), 'r')
+        response = self.client.post('/caffe/import', {'file': sample_file})
+        response = json.loads(response.content)
+        print 'Test {:}!'.format(response['result'])
