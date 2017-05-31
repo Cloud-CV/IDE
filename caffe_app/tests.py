3d6ba53eca0476c1aacaceeba94c9379dabe12e3
utsavgarg <utsav1996@ymail.com>
2017-05-31 14:11:15 +0530

    Added initial unit test to check file upload

--- caffe_app/tests.py
+++ caffe_app/tests.py
@@ -1,1 +1,15 @@
-# Create your tests here.
+from django.test import Client
+from django.conf import settings
+import unittest
+import json
+import os
+
+class UploadTest(unittest.TestCase):
+		def setUp(self):
+				self.client = Client()
+
+		def test_details(self):
+				sample_file = open(os.path.join(settings.BASE_DIR,'example','GoogleNet.prototxt'), 'r')
+				response = self.client.post('/caffe/import', {'file': sample_file})
+				response = json.loads(response.content)
+				print 'Test {:}!'.format(response['result'])
