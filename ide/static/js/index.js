646fafe0c101de6cef623822fedea36405c6f0fd
utsavgarg <utsav1996@ymail.com>
2017-08-08 10:51:27 +0800

    changed to browserHistory

--- ide/static/js/index.js
+++ ide/static/js/index.js
@@ -1,13 +1,13 @@
 
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, hashHistory } from 'react-router';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './app.js';
 import '../css/style.css';
 
 
 render(
-  <Router history={hashHistory}>
+  <Router history={browserHistory}>
     <Route path="/" component={App} />
   </Router>, document.getElementById('app')
 );
