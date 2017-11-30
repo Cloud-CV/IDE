09d5d0426c0577a4aa0c58fa94dc05d998aff57f
Will Eggleston <work.willeggleston@gmail.com>
2017-11-30 11:20:14 -0600

    Added Google Analytics with dummy ID

--- ide/settings.py
+++ ide/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'caffe_app.apps.CaffeAppConfig',
     'keras_app.apps.KerasAppConfig',
     'tensorflow_app.apps.TensorflowAppConfig',
+    'google_analytics',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
