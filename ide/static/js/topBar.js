d024873b2321b3c37babce4af866b568ec5958d2
Yash Dusing <yashdusing145@gmail.com>
2018-03-17 23:46:06 +0530

    Added Multiple Deletion Functionality to Action Branch

--- ide/static/js/topBar.js
+++ ide/static/js/topBar.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactTooltip from 'react-tooltip';
+import data from './data';
 
 class TopBar extends React.Component {
+  isButtonDisabled(layer) {
+    if (layer)
+      return (layer.info.phase === null) && (this.props.selectedPhase === 1) && (data[layer.info.type].learn);
+    else return false;
+  }
+
   render() {
+    var delete_button_options = null;
+    var delete_button_data_tip = "Select a layer first"
+    if (this.props.selectedLayer) {
+      const layer = this.props.net[this.props.selectedLayer];
+      var class_options = "btn";
+      if ((layer.info.phase === null) && (this.props.selectedPhase === 1) && (data[layer.info.type].learn))
+        class_options = "btn disabled";
+      delete_button_options = (<ul className="dropdown-menu dropdown-menu-right">
+        <li><a className={class_options} href="#" onClick={() => this.props.store_list_layers(this.props.selectedLayer)}>
+        Add selected layer to 'delete' queue</a></li>
+        <li><a className={class_options} href="#" onClick={() => this.props.delete_selected_layers()}>
+        Empty 'delete' queue</a></li>
+      </ul>);
+      delete_button_data_tip = "";
+    }
+
     return (
       <div className="topBar">
         <div className="topbar-row">
@@ -78,6 +101,17 @@ class TopBar extends React.Component {
                 </button>
               </div>
             </div>
+            <div className="topbar-col">
+              <div className="form-group">
+                <div className="dropdown">
+                  <button id="topbar-icon" className="btn btn-default dropdown-toggle form-control float-left"  data-toggle="dropdown"
+                   data-tip={delete_button_data_tip}>
+                    <span className="	glyphicon glyphicon-remove" aria-hidden="true"></span>
+                  </button>
+                  {delete_button_options}
+                </div>
+              </div>
+            </div>
         </div>
       <ReactTooltip type="dark" multiline={true}/>
       </div>
@@ -86,13 +120,18 @@ class TopBar extends React.Component {
 }
 
 TopBar.propTypes = {
+  selectedLayer: React.PropTypes.string,
+  net: React.PropTypes.object,
+  selectedPhase: React.PropTypes.number,
   exportNet: React.PropTypes.func,
   importNet: React.PropTypes.func,
   saveDb: React.PropTypes.func,
   loadDb: React.PropTypes.func,
   zooModal: React.PropTypes.func,
   textboxModal: React.PropTypes.func,
-  urlModal: React.PropTypes.func
+  urlModal: React.PropTypes.func,
+  store_list_layers: React.PropTypes.func,
+  delete_selected_layers: React.PropTypes.func
 };
 
 export default TopBar;
