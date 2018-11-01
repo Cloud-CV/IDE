03115b84d321b8af168a024c962fbe56256a768f
ZeroZeroJedenJeden <44526720+ZeroZeroJedenJeden@users.noreply.github.com>
2018-11-01 17:40:54 +0100

    Add files via upload

--- ide/static/js/pane.js
+++ ide/static/js/pane.js
@@ -6,6 +6,7 @@ class Pane extends React.Component {
   constructor(props) {
         super(props);
         this.toggleClass= this.toggleClass.bind(this);
+        this.changeEvent= this.changeEvent.bind(this);
         this.state = {
             data: false,
             vision: false,
@@ -68,6 +69,7 @@ class Pane extends React.Component {
             elem.style.display = 'none';
           }
         }
+      this.changeEvent;
         for (let elem of $('.panel-heading')) {
           let _p = pattern ? 'false' : 'true';
           if (elem.getAttribute('aria-expanded') == _p) {
@@ -80,8 +82,49 @@ class Pane extends React.Component {
       });
     }
 
+changeEvent() {
+      var KerasLayers = ["RNN_Button", "GRU_Button", "LSTM_Button", "Embed_Button", "Eltwise_Button", "ThresholdedReLU", "ReLU_Button", "PReLU_Button", "Softmax_Button", "BatchNorm_Button", "GaussianNoise_Button", "GaussianDropout_Button", "AlphaDropout_Button", "TimeDistributed_Button", "Bidirectional_Button", "RepeatVector_Button", "Masking_Button", "Permute_Button", "InnerProduct_Button", "Deconvolution_Button", "Regularization_Button", "Softsign_Button", "Upsample_Button", "Pooling_Button", "LocallyConnected_Button"];
+      var TensorFlowLayers = ["RNN_Button", "GRU_Button", "LSTM_Button", "Embed_Button", "Eltwise_Button", "ThresholdedReLU", "ReLU_Button", "PReLU_Button", "Softmax_Button", "BatchNorm_Button", "GaussianNoise_Button", "GaussianDropout_Button", "AlphaDropout_Button", "TimeDistributed_Button", "Bidirectional_Button", "RepeatVector_Button", "Masking_Button", "Permute_Button", "InnerProduct_Button", "Deconvolution_Button", "Regularization_Button", "Softsign_Button", "Upsample_Button", "Pooling_Button", "LocallyConnected_Button", "SoftmaxWithLoss_Button", "SigmoidCrossEntropyLoss_Button"];
+      var CaffeLayers = ["ImageData_Button", "HDF5Data_Button", "HDF5Output_Button", "Input_Button", "WindowData_Button", "MemoryData_Button", "DummyData_Button", "Convolution_Button", "Pooling_Button", "SPP_Button", "Deconvolution_Button", "Recurrent_Button", "RNN_Button", "LSTM_Button", "LRN_Button", "MVN_Button", "BatchNorm_Button", "InnerProduct_Button", "Dropout_Button", "Embed_Button", "ReLU_Button", "PReLU_Button", "ELU_Button", "Sigmoid_Button", "TanH_Button", "AbsVal_Button", "Power_Button", "Exp_Button", "Log_Button", "BNLL_Button", "Threshold_Button", "Bias_Button", "Scale_Button", "Softplus_Button", "HardSigmoid_Button", "Flatten_Button", "Reshape_Button", "BatchReindex_Button", "Split_Button", "Concat_Button", "Eltwise_Button", "Filter_Button", "Reduction_Button", "Silence_Button", "ArgMax_Button", "Softmax_Button", "MultinomialLogisticLoss_Button", "InfogainLoss_Button", "SoftmaxWithLoss_Button", "EuclideanLoss_Button", "HingeLoss_Button", "SigmoidCrossEntropyLoss_Button", "Accuracy_Button", "ContrastiveLoss_Button", "Data_Button"];
+      var CheckBoxA = this.refs.CheckBoxA;
+      var CheckBoxB = this.refs.CheckBoxB;
+      var CheckBoxC = this.refs.CheckBoxC;
+      var visible = [];
+      if(CheckBoxA.checked == false & CheckBoxB.checked == false & CheckBoxC.checked == false){
+        for (let elem of $('.drowpdown-button')) {
+        elem.classList.remove("hide");
+        }            
+      }
+      if (this.refs.CheckBoxA.checked == true){
+         visible = visible.concat(KerasLayers);
+      }
+      if (this.refs.CheckBoxB.checked == true){
+         visible = visible.concat(TensorFlowLayers);
+      }
+      if (this.refs.CheckBoxC.checked == true){
+         visible = visible.concat(CaffeLayers);
+      }
+      
+            for (let elem of $('.drowpdown-button')) {
+                for (let j = 0; j < visible.length; j++){
+                let id = elem.id;
+                if(id == visible[j]){
+                    elem.classList.remove("hide");
+                    j = visible.length + 1;
+                }else{
+                elem.classList.add("hide");
+                }
+            }
+        }
+
+    }
+
     render() {
       return (
+            <div>
+            <div><input type="checkbox" id="CheckBoxA" ref="CheckBoxA" value="A" onChange={this.changeEvent} /><label>Keras</label></div>
+            <div><input type="checkbox" id="CheckBoxB" ref="CheckBoxB" value="B" onChange={this.changeEvent} /><label>TensorFlow</label></div>
+            <div><input type="checkbox" id="CheckBoxC" ref="CheckBoxC" value="C" onChange={this.changeEvent} /><label>Caffe</label></div>
         <div className="panel-group" id="menu" role="tablist" aria-multiselectable="true">
               <div className="panel panel-default">
                 <div href="#data" data-toggle="collapse" aria-expanded="false" aria-controls="data"
@@ -460,7 +503,7 @@ class Pane extends React.Component {
                 </div>
               </div>
         </div>
-
+</div>
 
       );
   }
