7e197ff0d3be75dfc4a1c28e8f4dad9050be8304
ZeroZeroJedenJeden <44526720+ZeroZeroJedenJeden@users.noreply.github.com>
2018-11-02 07:39:19 +0100

    Add files via upload

--- ide/static/js/pane.js
+++ ide/static/js/pane.js
@@ -88,13 +88,13 @@ changeEvent() {
       "GaussianNoise_Button", "GaussianDropout_Button", "AlphaDropout_Button", "TimeDistributed_Button",
       "Bidirectional_Button", "RepeatVector_Button", "Masking_Button", "Permute_Button", "InnerProduct_Button",
       "Deconvolution_Button", "Regularization_Button", "Softsign_Button", "Upsample_Button", "Pooling_Button",
-      "LocallyConnected_Button"];
+      "LocallyConnected_Button", "Crop_Button"];
       var TensorFlowLayers = ["RNN_Button", "GRU_Button", "LSTM_Button", "Embed_Button", "Eltwise_Button",
       "ThresholdedReLU", "ReLU_Button", "PReLU_Button", "Softmax_Button", "BatchNorm_Button", "GaussianNoise_Button",
       "GaussianDropout_Button", "AlphaDropout_Button", "TimeDistributed_Button", "Bidirectional_Button",
       "RepeatVector_Button", "Masking_Button", "Permute_Button", "InnerProduct_Button", "Deconvolution_Button",
       "Regularization_Button", "Softsign_Button", "Upsample_Button", "Pooling_Button", "LocallyConnected_Button",
-      "SoftmaxWithLoss_Button", "SigmoidCrossEntropyLoss_Button"];
+      "SoftmaxWithLoss_Button", "SigmoidCrossEntropyLoss_Button", "Crop_Button", "DepthwiseConv_Button"];
       var CaffeLayers = ["ImageData_Button", "HDF5Data_Button", "HDF5Output_Button", "Input_Button", "WindowData_Button",
       "MemoryData_Button", "DummyData_Button", "Convolution_Button", "Pooling_Button", "SPP_Button", "Deconvolution_Button",
       "Recurrent_Button", "RNN_Button", "LSTM_Button", "LRN_Button", "MVN_Button", "BatchNorm_Button",
@@ -104,23 +104,23 @@ changeEvent() {
       "Reshape_Button", "BatchReindex_Button", "Split_Button", "Concat_Button", "Eltwise_Button", "Filter_Button",
       "Reduction_Button", "Silence_Button", "ArgMax_Button", "Softmax_Button", "MultinomialLogisticLoss_Button",
       "InfogainLoss_Button", "SoftmaxWithLoss_Button", "EuclideanLoss_Button", "HingeLoss_Button",
-      "SigmoidCrossEntropyLoss_Button", "Accuracy_Button", "ContrastiveLoss_Button", "Data_Button"];
-      var CheckBoxA = this.refs.CheckBoxA;
-      var CheckBoxB = this.refs.CheckBoxB;
-      var CheckBoxC = this.refs.CheckBoxC;
+      "SigmoidCrossEntropyLoss_Button", "Accuracy_Button", "ContrastiveLoss_Button", "Data_Button", "Crop_Button"];
+      var CheckBoxA = document.getElementById("CheckBoxA");
+      var CheckBoxB = document.getElementById("CheckBoxB");
+      var CheckBoxC = document.getElementById("CheckBoxC");
       var visible = [];
       if(CheckBoxA.checked == false & CheckBoxB.checked == false & CheckBoxC.checked == false){
         for (let elem of $('.drowpdown-button')) {
         elem.classList.remove("hide");
         }            
       }
-      if (this.refs.CheckBoxA.checked == true){
+      if (CheckBoxA.checked == true){
          visible = visible.concat(KerasLayers);
       }
-      if (this.refs.CheckBoxB.checked == true){
+      if (CheckBoxB.checked == true){
          visible = visible.concat(TensorFlowLayers);
       }
-      if (this.refs.CheckBoxC.checked == true){
+      if (CheckBoxC.checked == true){
          visible = visible.concat(CaffeLayers);
       }
       
@@ -141,9 +141,34 @@ changeEvent() {
     render() {
       return (
             <div>
-            <div><input type="checkbox" id="CheckBoxA" ref="CheckBoxA" value="A" onChange={this.changeEvent} /><label>Keras</label></div>
-            <div><input type="checkbox" id="CheckBoxB" ref="CheckBoxB" value="B" onChange={this.changeEvent} /><label>TensorFlow</label></div>
-            <div><input type="checkbox" id="CheckBoxC" ref="CheckBoxC" value="C" onChange={this.changeEvent} /><label>Caffe</label></div>
+              <div className="form-group">
+                <div className="filter dropdown">
+                  <button id="fiter-button" className="filter btn btn-default dropdown-toggle form-control" data-toggle="dropdown">
+                    <span className="glyphicon glyphicon-list-alt" aria-hidden="true"><label>Filters</label></span>
+                  </button>
+                  <ul className="dropdown-menu">
+                    <li>
+                        <a className="btn">
+                        <input type="checkbox" id="CheckBoxA" value="A" onChange={this.changeEvent} />
+                        <label>Keras</label>
+                        </a>
+                    </li>
+                    <li>
+                        <a className="btn">
+                        <input type="checkbox" id="CheckBoxB" value="B" onChange={this.changeEvent} />
+                        <label>Tensorflow</label>
+                        </a>
+                    </li>
+                    <li>
+                        <a className="btn">
+                        <input type="checkbox" id="CheckBoxC" value="C" onChange={this.changeEvent} />
+                        <label>Caffe</label>
+                        </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+
         <div className="panel-group" id="menu" role="tablist" aria-multiselectable="true">
               <div className="panel panel-default">
                 <div href="#data" data-toggle="collapse" aria-expanded="false" aria-controls="data"
