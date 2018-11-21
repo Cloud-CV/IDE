ae22fb5253fe179ead9df205f3ccaa4d53b8c5a9
C0der Lint <iCame.iLearnt.iExploited@gmail.com>
2018-11-21 02:01:23 -0500

    Updated forms.py

--- ide/forms.py
+++ ide/forms.py
@@ -2,9 +2,23 @@ from allauth.account.forms import SignupForm
 from django import forms
 
 class FabrikSignupForm(SignupForm):
-    username.widget.attrs.update({'class': 'validate'})
-    email.widget.attrs.update({'class': 'validate'})
-    password1.widget.attrs.update({'class': 'validate'})
+
+    def __init__(self, *args, **kwargs):
+        super(FabrikSignupForm, self).__init__(*args, **kwargs)
+    	
+        username = self.fields['username']
+        email = self.fields['email']
+        password1 = self.fields['password1']
+        password2 = self.fields['password2']
+
+        username.widget.attrs.update({'class': 'validate'})
+        email.widget.attrs.update({'class': 'validate'})
+        password1.widget.attrs.update({'class': 'validate'})
+        
+        username.label = "Username"
+        email.label = "Email"
+        password1.label = "Password"
+        password2.label = "Confirm Password"
 
     def signup(self, request, user):
         user.save()
